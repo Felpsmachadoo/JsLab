50d283c51297c1f1c5b0f68fc702c71eab119f33
Felipe Machado <felpsmachadoo@gmail.com>
2025-03-30 18:08:57 -0300

    Atualizando Tarefas

--- semana4 copy/exercicios/exercicio1/classes/aluno.js
+++
@@ -1,6 +0,0 @@
-class Aluno extends Pessoa {
-  constructor(nome, email, date, homePhone, cellPhone, matricula, curso) {
-    super(nome, email, date, homePhone, cellPhone, matricula);
-    this.curso = curso;
-  }
-}
